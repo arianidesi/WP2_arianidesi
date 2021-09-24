ed39390ba3105a8a039676b59409355b53323553
arianidesi <arianidesi070302@gmail.com>
2021-09-24 16:15:33 +0700

    upload awal project

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+<head>
+    
+<body>
+    Halo kawan .. yuk kita belajar web Programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
