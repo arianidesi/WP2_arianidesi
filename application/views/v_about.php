e72b95a08e1955d5bf3869859afd435b682644e9
arianidesi <arianidesi070302@gmail.com>
2021-09-28 19:45:31 +0700

    membuat template sederhana yang dinamis

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ariani</li>
+        <li>Nama Belakang : Desi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan sungai raya dalam</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Sunsang</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Joging</li>
+        <li>Renang</li>
+    </ul>
+</section>
